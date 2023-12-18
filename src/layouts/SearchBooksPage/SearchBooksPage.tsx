9a7fbd4a189b52075aff9725f3122000c090c5be
hayate0721 <h08066802495@gmail.com>
2023-12-18 09:42:01 +0900

    bug fixe for search

--- src/layouts/SearchBooksPage/SearchBooksPage.tsx
+++ src/layouts/SearchBooksPage/SearchBooksPage.tsx
@@ -27,7 +27,8 @@ export const SearchBooksPage = () => {
             if(searchUrl === ''){
                 url = `${baseUrl}?page=${currentPage - 1}&size=${booksPerPage}`;
             }else{
-                url = baseUrl + searchUrl;
+                let searchWithPage = searchUrl.replace('<pageNumber>', `${currentPage - 1}`);
+                url = baseUrl + searchWithPage;
             }
 
             const response  = await fetch(url);
@@ -83,14 +84,19 @@ export const SearchBooksPage = () => {
     }
 
     const searchHandleChange = () => {
+        setCurrentPage(1);
+
         if(search === ''){
             setSearchUrl('');
         }else{
-            setSearchUrl(`/search/findByTitleContaining?title=${search}&page=0&size=${booksPerPage}`)
+            setSearchUrl(`/search/findByTitleContaining?title=${search}&page=<pageNumber>&size=${booksPerPage}`)
         }
+
+        setCategorySelection('Book category');
     }
 
     const categoryField = (value: string) => {
+        setCurrentPage(1);
         if(
             value.toLowerCase() === 'fe' ||
             value.toLowerCase() === 'be' ||
@@ -98,10 +104,10 @@ export const SearchBooksPage = () => {
             value.toLowerCase() === 'devops'
         ){
             setCategorySelection(value);
-            setSearchUrl(`/search/findByCategory?category=${value}&page=0&size=${booksPerPage}`)
+            setSearchUrl(`/search/findByCategory?category=${value}&page=<pageNumber>&size=${booksPerPage}`)
         }else{
             setCategorySelection('All');
-            setSearchUrl(`?page=0&size=${booksPerPage}`)
+            setSearchUrl(`?page=<pageNumber>&size=${booksPerPage}`)
         }
 
     }
